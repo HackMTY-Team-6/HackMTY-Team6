bb5c5e966b2e1778044a494d300a63927046a8b9
Santiago Reyes Moran <sannaga1995@gmail.com>
2022-09-24 16:45:20 -0500

    feat(server): conexion con back4app

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Parse = require("./utils/parse_config");
+
 
 app.use(express.json());
 app.use(cors());
@@ -9,8 +11,20 @@ app.use(cors());
 
 app.get('/test',(req,res)=>{
     res.send("Si sirve");
-    
+
     });
+
+app.get('/test2', async (req,res)=> {
+
+    const Usuarios = Parse.Object.extend("Usuarios");
+    const query = new Parse.Query(Usuarios);
+    const response=await query.find();
+    //console.log(response);
+    res.send(response);
+
+
+
+});
 
 module.exports = app;
 
