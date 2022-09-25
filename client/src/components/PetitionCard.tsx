9eaf404773103995843167de406174eed2fdccc1
CristianCazares <cm_cjavier@hotmail.com>
2022-09-25 07:07:50 -0500

    feat(theme): disable dark themes

--- client/src/components/PetitionCard.tsx
+++ client/src/components/PetitionCard.tsx
@@ -35,9 +35,8 @@ function PetitionCard({ petitionID, userID, date, place }: Props) {
     <div
       className="
       block p-6 max-w-sm bg-white rounded-lg border 
-      border-gray-200 shadow-md hover:bg-gray-100 
-      dark:bg-gray-800 dark:border-gray-700 
-      dark:hover:bg-gray-700 cursor-pointer relative"
+      border-gray-200 shadow-md hover:bg-gray-100
+      cursor-pointer relative"
     >
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
         {user?.nombreCompleto}
