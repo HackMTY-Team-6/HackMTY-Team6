173d4e6422ea5c5d8a3e7e2f8266f4639a15628c
CristianCazares <cm_cjavier@hotmail.com>
2022-09-25 11:40:38 -0500

    feat(petitionView): Added logic to render either users current petitions or gernal userss options

--- client/src/components/PeticionForm.tsx
+++ client/src/components/PeticionForm.tsx
@@ -1,53 +1,60 @@
-import axios from 'axios';
-import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 import { BiDonateBlood, BiDonateHeart } from "react-icons/bi";
-import { useNavigate } from 'react-router-dom';
-import { API_URL } from '../lib/constants';
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../lib/constants";
+import PetitionView from "./PetitionView";
 
 const PeticionForm = () => {
-    const navigate = useNavigate();
-    const [userID, setUserID] = useState("");
-    useEffect(() => {
-        const getUserID = (sessionToken: string) => {
-          axios
-            .get(`${API_URL}/userInfoWithSessionToken`, {
-              headers: {
-                "Content-Type": "application/json",
-                authorization: sessionToken,
-              },
-            })
-            .then((res) => {
-              setUserID(res.data[0].objectId);
-            });
-        };
-    
-        getUserID(localStorage.getItem("session_key"));
-      }, []);
-    
-    
+  const navigate = useNavigate();
+  const [hasPetition, setHasPetition] = useState(false);
+  useEffect(() => {
+    const getUserHasPetition = (sessionToken: string) => {
+      axios
+        .get(`${API_URL}/userInfoWithSessionToken`, {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: sessionToken,
+          },
+        })
+        .then((res) => {
+          const uID = res.data[0].objectId;
 
-    return (
-        <div className='px-[350px] py-[100px] mx-auto'>
-            <div className='flex gap-9 justify-between'>
+          axios.get(`${API_URL}/userPetitions/${uID}`).then((res) => {
+            if (res.data) {
+              setHasPetition(true);
+            }
+          });
+        });
+    };
+    getUserHasPetition(localStorage.getItem("session_key"));
+  }, []);
 
-                <div className='bg-slate-100 h-[200px] w-[350px] text-center shadow-lg transform transition duration-500 hover:scale-110 cursor-pointer hover:border-l-red-500 hover:border-l-8 rounded-lg'
-                    onClick={() => navigate("/donar")}
-                >
-                    <BiDonateBlood className='text-8xl mx-auto mt-5' />
-                    <p className='font-semibold mt-5 text-lg'>Donar</p>
-                </div>
-
-                <div className='bg-slate-100 h-[200px] w-[350px] text-center shadow-lg transform transition duration-500 hover:scale-110 cursor-pointer hover:border-l-red-500 hover:border-l-8 rounded-lg'
-                    onClick={() => navigate("/peticion")}
-                >
-                    <BiDonateHeart className='text-8xl mx-auto mt-5' />
-                    <p className='font-semibold mt-5 text-lg'>Pedir donación</p>
-                </div>
-
-            </div>
+  const options = (
+    <div className="px-[350px] py-[100px] mx-auto">
+      <div className="flex gap-9 justify-between">
+        <div
+          className="bg-slate-100 h-[200px] w-[350px] text-center shadow-lg transform transition duration-500 hover:scale-110 cursor-pointer hover:border-l-red-500 hover:border-l-8 rounded-lg"
+          onClick={() => navigate("/donar")}
+        >
+          <BiDonateBlood className="text-8xl mx-auto mt-5" />
+          <p className="font-semibold mt-5 text-lg">Donar</p>
         </div>
-    )
-}
 
-export default PeticionForm
+        <div
+          className="bg-slate-100 h-[200px] w-[350px] text-center shadow-lg transform transition duration-500 hover:scale-110 cursor-pointer hover:border-l-red-500 hover:border-l-8 rounded-lg"
+          onClick={() => navigate("/peticion")}
+        >
+          <BiDonateHeart className="text-8xl mx-auto mt-5" />
+          <p className="font-semibold mt-5 text-lg">Pedir donación</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const petition = <PetitionView />;
+  return <div>{hasPetition ? petition : options}</div>;
+};
+
+export default PeticionForm;
--- client/src/components/PetitionView.tsx
+++ client/src/components/PetitionView.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { API_URL } from "../lib/constants";
+import { API_URL, SESSION_KEY } from "../lib/constants";
 
 function PetitionView() {
   const [count, setCount] = useState(0);
-  const [userID, setUserID] = useState("");
+  const [petition, setPetition] = useState({});
+  const [bloodType, setBloodType] = useState("");
   useEffect(() => {
-    const getUserID = (sessionToken: string) => {
+    const getPetition = (sessionToken: string) => {
       axios
         .get(`${API_URL}/userInfoWithSessionToken`, {
           headers: {
@@ -15,17 +16,36 @@ function PetitionView() {
           },
         })
         .then((res) => {
-          setUserID(res.data[0].objectId);
+          const uID = res.data[0].objectId;
+          setBloodType(res.data[0].tipoSangre);
+          console.log(res.data[0]);
+          axios.get(`${API_URL}/userPetitions/${uID}`).then((res) => {
+            const pID = res.data.peticionID;
+            axios.get(`${API_URL}/petitionInfo/${pID}`).then((res) => {
+              setCount(res.data[0].views);
+              setPetition(res.data[0]);
+            });
+          });
         });
     };
-
-    getUserID(localStorage.getItem("session_key"));
+    getPetition(localStorage.getItem(SESSION_KEY));
   }, []);
 
-  const stringCount =
-    count === 0
-      ? "Aún no encontramos a algún donador para tu solicitud"
-      : `${count} se han postulador para donar. Les hemos compartido tu inormación de contacto.`;
+  let stringCountMessages = ["", "", ""];
+  if (count === 0) {
+    stringCountMessages[0] =
+      "Aún no encontramos a algún donador para tu solicitud";
+  } else if (count === 1) {
+    stringCountMessages[0] = `Hemos encontrado `;
+    stringCountMessages[1] = " persona dispuesta a donar.";
+    stringCountMessages[2] =
+      "Se le compartió tu información de contacto para dar seguimiento.";
+  } else {
+    stringCountMessages[0] = `Hemos encontrado `;
+    stringCountMessages[1] = "personas dispuesta a donar.";
+    stringCountMessages[2] =
+      "Se le compartió tu información de contacto para dar seguimiento.";
+  }
 
   return (
     <div className="w-full p-[100px]">
@@ -34,7 +54,17 @@ function PetitionView() {
           <h1 className="mx-auto text-4xl font-semibold text-green-800 text-center">
             Petición de donativo
           </h1>
-          <p className="text-3xl">{stringCount}</p>
+          <p className="text-2xl">
+            {stringCountMessages[0]}
+            <span className="font-bold text-red-600">{count}</span>
+            {stringCountMessages[1]}
+          </p>
+          <p>{stringCountMessages[2]}</p>
+          <br></br>
+          <p className="font-bold text-green-800">Resumen de petición:</p>
+          <p className="font-bold">🩸{bloodType}</p>
+          <p className="font-bold">{petition.fecha}</p>
+          <p className="font-bold">{petition.lugarParaRecibir}</p>
         </div>
       </div>
     </div>
